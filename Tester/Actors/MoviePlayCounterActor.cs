de526ca2ee5eb70dce5bbbcbe8ff4f3cdaf2aa56
Pavel Svarc <pavel.svarc@ipl.com>
2017-02-15 07:17:06 +0000

    Updated implementation for movie play counter

--- Tester/Actors/MoviePlayCounterActor.cs
+++ Tester/Actors/MoviePlayCounterActor.cs
@@ -1,14 +1,34 @@
 ﻿using Akka.Actor;
 using System;
+using System.Collections.Generic;
+using Tester.Messages;
 
 namespace Tester.Actors
 {
     public class MoviePlayCounterActor : ReceiveActor
     {
+        private readonly Dictionary<string, int> _moviePlayCounts;
+
         public MoviePlayCounterActor()
         {
-
+            _moviePlayCounts = new Dictionary<string, int>();
+            Receive<IncrementPlayCountMessage>(message => HandleIncrementCount(message));
         }
+
+        private void HandleIncrementCount(IncrementPlayCountMessage message)
+        {
+            if (!_moviePlayCounts.ContainsKey(message.MovieTitle))
+            {
+                _moviePlayCounts.Add(message.MovieTitle, 1);
+            }
+            else
+            {
+                _moviePlayCounts[message.MovieTitle]++;
+            }
+
+            ConsoleLogger.LogMessage($"Movie: {message.MovieTitle} has been played {_moviePlayCounts[message.MovieTitle]}");
+        }
+
         #region Lifetime hooks
         protected override void PreStart()
         {
---
+++ Tester/Messages/IncrementPlayCounMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester.Messages
+{
+    public class IncrementPlayCountMessage
+    {
+        public string MovieTitle { get; private set; }
+
+        public IncrementPlayCountMessage(string movieTitle)
+        {
+            this.MovieTitle = movieTitle;
+        }
+    }
+}
